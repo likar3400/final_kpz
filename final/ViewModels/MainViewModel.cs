fac93258cb516b903dab9073ba191575d3f9d9c1
marryysa12-sketch <marryysa12@gmail.com>
2026-05-03 20:41:05 +0300

    Use search strategy in main view model

--- final/ViewModels/MainViewModel.cs
+++ final/ViewModels/MainViewModel.cs
@@ -5,48 +5,50 @@ using RecipeFinder.Services;
 
 namespace RecipeFinder.ViewModels
 {
-	public class MainViewModel : BaseViewModel
-	{
-		private readonly IRecipeRepository _repository;
-		private ObservableCollection<Recipe> _recipes;
-		private string _searchText;
+    public class MainViewModel : BaseViewModel
+    {
+        private readonly IRecipeRepository _repository;
+        private readonly ISearchStrategy _searchStrategy;
+        private ObservableCollection<Recipe> _recipes;
+        private string _searchText;
 
-		public MainViewModel(IRecipeRepository repository)
-		{
-			_repository = repository;
-			_recipes = new ObservableCollection<Recipe>(_repository.GetAll());
+        public MainViewModel(IRecipeRepository repository)
+        {
+            _repository = repository;
+            _searchStrategy = new NameSearchStrategy();
+            _recipes = new ObservableCollection<Recipe>(_repository.GetAll());
 
-			SearchCommand = new RelayCommand(obj => ExecuteSearch());
-			AddCommand = new RelayCommand(obj => OpenEditor());
-		}
+            SearchCommand = new RelayCommand(obj => ExecuteSearch());
+            AddCommand = new RelayCommand(obj => OpenEditor());
+        }
 
-		public ObservableCollection<Recipe> Recipes
-		{
-			get => _recipes;
-			set => SetProperty(ref _recipes, value);
-		}
+        public ObservableCollection<Recipe> Recipes
+        {
+            get => _recipes;
+            set => SetProperty(ref _recipes, value);
+        }
 
-		public string SearchText
-		{
-			get => _searchText;
-			set
-			{
-				if (SetProperty(ref _searchText, value))
-					ExecuteSearch(); 
-			}
-		}
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                if (SetProperty(ref _searchText, value))
+                {
+                    ExecuteSearch();
+                }
+            }
+        }
 
-		public ICommand SearchCommand { get; }
-		public ICommand AddCommand { get; }
+        public ICommand SearchCommand { get; }
+        public ICommand AddCommand { get; }
 
-		private void ExecuteSearch()
-		{
-			// Логіка пошуку
-			var filtered = _repository.GetAll()
-				.Where(r => r.Title.Contains(SearchText, StringComparison.OrdinalIgnoreCase));
-			Recipes = new ObservableCollection<Recipe>(filtered);
-		}
+        private void ExecuteSearch()
+        {
+            var filtered = _searchStrategy.Filter(_repository.GetAll(), SearchText);
+            Recipes = new ObservableCollection<Recipe>(filtered);
+        }
 
-		private void OpenEditor() { /* Логіка відкриття вікна створення */ }
-	}
+        private void OpenEditor() { /* Р›РѕРіС–РєР° РІС–РґРєСЂРёС‚С‚СЏ РІС–РєРЅР° СЃС‚РІРѕСЂРµРЅРЅСЏ */ }
+    }
 }
